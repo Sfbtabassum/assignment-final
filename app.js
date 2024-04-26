866fa392ac2b66dc2565f1a6602a6bd6e28d582c
Sfbtabassum <158295877+Sfbtabassum@users.noreply.github.com>
2024-04-26 12:12:47 +0600

    api

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const showAllButton = document.getElementById('showAllButton');
 
 let meals = [];
 let visibleMeals = [];
+
+
+async function fetchMeals(searchText) {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`);
+  const data = await response.json();
+  return data.meals;
+}
